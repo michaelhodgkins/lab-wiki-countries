56c7ead876fc0c738b3caecce20bbaba79ecaa2f
Michael Hodgkins <mhodgkins1997@gmail.com>
2022-09-04 21:41:33 +0100

    single list displaying but not for specific id

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,14 +1,19 @@
 import { useParams } from 'react-router-dom'
 
+
 function CountryDetails({countries}) {
     const { countryId } = useParams();
     const country = countries.find((a) => a.alpha3code === countryId);
-console.log(country)
+console.log(countries)
 
     return (
         <div>  
-            <h1>Details</h1>
-            <p>{country}</p>
+            <h1>{country.name.common}</h1>
+            <h2>{country.alpha2code}</h2>
+            <p>Region: {country.region}</p>
+            <p>Languages: {country.languages.nld}</p>
+            <p>Currencies: {country.currencies.AWG.name}</p>
+            <p></p>
         </div>
     )}
     
